3179e62115d08ff67a2201fc3cf64aaedbd9f97f
sihabhossain <sihabhossain380@gmail.com>
2024-09-28 21:32:04 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/Auth/auth.route';
+import { UserRoutes } from '../modules/User/user.route';
+import { ProfileRoutes } from '../modules/Profile/profile.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/profile',
+    route: ProfileRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+export default router;
