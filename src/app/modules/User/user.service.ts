ccc0ca4c263f03980d4986dd1b22eefe3bbbc8b6
sihabhossain <sihabhossain380@gmail.com>
2024-10-11 21:19:48 +0600

    url added

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -146,8 +146,8 @@ const createCheckoutSession = async (payload: { priceId: string }) => {
       },
     ],
     mode: 'payment',
-    success_url: `${config.stripe_success_url}`,
-    cancel_url: `${config.stripe_cancel_url}`,
+    success_url: `https://flavor-exchange.netlify.app/dashboard/stripe/success`,
+    cancel_url: `https://flavor-exchange.netlify.app/dashboard/stripe/cancel`,
   });
 
   return session;
